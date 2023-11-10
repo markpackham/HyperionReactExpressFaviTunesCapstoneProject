5f500e21309439def3a06c360ef61de552241224
mark <markpackham1@gmail.com>
2023-11-10 13:17:59 +0000

    use Dropdown for search filter

--- frontend/src/components/search-favs/FavSearch.jsx
+++ frontend/src/components/search-favs/FavSearch.jsx
@@ -4,25 +4,88 @@ import DOMPurify from "dompurify";
 import AddFav from "./AddFav";
 import RemoveFav from "./RemoveFav";
 
-const BookSearch = () => {
+const Dropdown = ({ options, value, onChange }) => {
+  const [show, setShow] = useState(false);
+
+  const handleToggle = () => {
+    setShow(!show);
+  };
+
+  const handleChange = (event) => {
+    onChange(event.target.value);
+    setShow(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <button className="dropdown-button" onClick={handleToggle}>
+        {value}
+      </button>
+      {show && (
+        <ul className="dropdown-list">
+          {options.map((option) => (
+            <li key={option.value}>
+              <input
+                type="radio"
+                id={option.value}
+                name="dropdown"
+                value={option.value}
+                checked={value === option.value}
+                onChange={handleChange}
+              />
+              <label htmlFor={option.value}>{option.label}</label>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+const FavSearch = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [items, setItems] = useState([]);
+  const [media, setMedia] = useState("ebook");
+  const [entity, setEntity] = useState("ebook");
+
+  const mediaOptions = [
+    { value: "ebook", label: "Ebook" },
+    { value: "music", label: "Music" },
+    { value: "movie", label: "Movie" },
+  ];
+
+  const entityOptions = {
+    ebook: [
+      { value: "ebook", label: "Ebook" },
+      { value: "audiobook", label: "Audiobook" },
+    ],
+    music: [
+      { value: "musicTrack", label: "Music Track" },
+      { value: "album", label: "Album" },
+      { value: "musicVideo", label: "Music Video" },
+    ],
+    movie: [
+      { value: "movie", label: "Movie" },
+      { value: "feature-movie", label: "Feature Movie" },
+      { value: "movieArtist", label: "Movie Artist" },
+    ],
+  };
 
   useEffect(() => {
     const fetchSearchResults = async () => {
       const response = await axios.get("https://itunes.apple.com/search", {
         params: {
           term: searchTerm,
-          media: "ebook",
-          entity: "ebook",
+          media: media,
+          entity: entity,
           limit: 7,
         },
       });
       setSearchResults(response.data.results);
     };
     fetchSearchResults();
-  }, [searchTerm]);
+  }, [searchTerm, media, entity]);
 
   const handleSearchTermChange = (event) => {
     // Prevent script injections
@@ -37,6 +100,15 @@ const BookSearch = () => {
     setItems(items.filter((it) => it.trackId !== it.trackId));
   };
 
+  const handleMediaChange = (value) => {
+    setMedia(value);
+    setEntity(entityOptions[value][0].value);
+  };
+
+  const handleEntityChange = (value) => {
+    setEntity(value);
+  };
+
   return (
     <div>
       <h1>Fav Media</h1>
@@ -47,6 +119,16 @@ const BookSearch = () => {
         onChange={handleSearchTermChange}
         placeholder="Search for something"
       />
+      <Dropdown
+        options={mediaOptions}
+        value={media}
+        onChange={handleMediaChange}
+      />
+      <Dropdown
+        options={entityOptions[media]}
+        value={entity}
+        onChange={handleEntityChange}
+      />
       <div className="row">
         <div className="col-sm-12 col-md-6 item-search">
           <ul>
@@ -56,6 +138,19 @@ const BookSearch = () => {
                   {result.trackName} by {result.artistName}
                 </a>
                 <AddFav item={result} onAdd={handleAddItem} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-sm-12 col-md-6 item-list">
+          <h3>Your fav list</h3>
+          <ul>
+            {items.map((item) => (
+              <li key={item.trackId}>
+                <a href={item.trackViewUrl} target="_blank" rel="noreferrer">
+                  {item.trackName} by {item.artistName}
+                </a>
+                <RemoveFav item={item} onRemove={handleRemoveItem} />
               </li>
             ))}
           </ul>
@@ -78,4 +173,4 @@ const BookSearch = () => {
   );
 };
 
-export default BookSearch;
+export default FavSearch;
