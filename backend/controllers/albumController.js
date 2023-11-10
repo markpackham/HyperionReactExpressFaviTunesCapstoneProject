9262bdedb796d680111cc00f022260556fa11135
mark <markpackham1@gmail.com>
2023-11-10 18:22:32 +0000

    avoid overwitting res

--- backend/controllers/albumController.js
+++ backend/controllers/albumController.js
@@ -2,19 +2,18 @@ const axios = require("axios");
 
 exports.albumInfo = async (req, res) => {
   const { albumName, artistName } = req.body;
-  console.log(albumName);
 
   const url = `https://itunes.apple.com/search?term=${albumName}+${artistName}&entity=album`;
   try {
-    const res = await axios.get(url);
-    const albums = res.data.results.map((album) => ({
+    const axiosResponse = await axios.get(url);
+    //Obtain all album info
+    const albums = axiosResponse.data.results.map((album) => ({
       albumName: album.collectionName,
       artistName: album.artistName,
       albumCoverImage: album.artworkUrl100,
       releaseDate: album.releaseDate,
     }));
-    console.log(albums);
-    res.send("All good");
+    res.status(200).send(albums);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
