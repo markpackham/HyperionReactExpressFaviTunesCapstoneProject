b09b591694e52b2e7e6b9817521bf6377fe545dd
mark <markpackham1@gmail.com>
2023-11-13 14:13:56 +0000

    add model on homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [show, setShowMoreInfo] = useState(false);
+
   return (
     <>
       <h2>Store All Your Fav Music, Movies & Books Right Here!</h2>
@@ -22,6 +26,31 @@ const Home = () => {
       <div className="lowerImage d-flex justify-content-center">
         <img src="../../../public/music_images/recordPlayer3.jpg" alt="." />
       </div>
+
+      <>
+        <Button variant="primary" onClick={() => setShowMoreInfo(true)}>
+          More Info
+        </Button>
+        <Modal show={show} onHide={() => setShowMoreInfo(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Body text</p>
+            <div className="lowerImage d-flex justify-content-center">
+              <img
+                src="../../../public/music_images/recordPlayer4.jpg"
+                alt="."
+              />
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowMoreInfo(false)}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     </>
   );
 };
