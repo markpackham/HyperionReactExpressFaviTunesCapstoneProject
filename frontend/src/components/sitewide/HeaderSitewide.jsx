1045558e1e1fe69809d74687f228208e1574928d
mark <markpackham1@gmail.com>
2023-11-15 11:39:02 +0000

    align image with header

--- frontend/src/components/sitewide/HeaderSitewide.jsx
+++ frontend/src/components/sitewide/HeaderSitewide.jsx
@@ -1,8 +1,14 @@
 const HeaderSitewide = () => {
   return (
-    <div>
-      <img src="/music_images/headphones3.jpg" alt="" id="logo" />
-      <h1 className="mt-2 mb-2">iTunes & Apple Books Search App</h1>
+    <div className="text-center">
+      <img
+        src="/music_images/headphones3.jpg"
+        alt=""
+        className="d-inline-block align-middle me-3"
+      />
+      <h1 className="d-inline-block align-middle mt-2 mb-2">
+        iTunes & Apple Books Search App
+      </h1>
     </div>
   );
 };
