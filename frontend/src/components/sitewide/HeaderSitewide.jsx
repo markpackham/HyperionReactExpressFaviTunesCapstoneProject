7f0bd9261190c36349ee7c475e5b5cde534a6e63
mark <markpackham1@gmail.com>
2023-11-15 11:36:44 +0000

    add logo image

--- frontend/src/components/sitewide/HeaderSitewide.jsx
+++ frontend/src/components/sitewide/HeaderSitewide.jsx
@@ -1,6 +1,7 @@
 const HeaderSitewide = () => {
   return (
     <div>
+      <img src="/music_images/headphones3.jpg" alt="" id="logo" />
       <h1 className="mt-2 mb-2">iTunes & Apple Books Search App</h1>
     </div>
   );
