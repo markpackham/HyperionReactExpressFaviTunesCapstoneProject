d615d1d023956ddeb1ad519188210061396077dd
mark <markpackham1@gmail.com>
2023-11-17 16:06:53 +0000

    test port for favs working

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -1,8 +1,11 @@
 const request = require("supertest");
-const app = require("../appTest");
 
-describe("Test the root path", () => {
-  test("It should response the GET method", () => {
-    return request(app).get("/").expect(200);
+// Unit testing learned from
+// Gao, A. (2022) How to test express.js with jest and Supertest, Through the binary.
+// Available at: https://www.albertgao.xyz/2017/05/24/how-to-test-expressjs-with-jest-and-supertest/ (Accessed: 17 November 2023).
+describe("Express server port check", () => {
+  it("should be running on port 8080", async () => {
+    const response = await request("http://localhost:8080").get("/favs");
+    expect(response.status).toBe(200);
   });
 });
