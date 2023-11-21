cf0f03e639ee653d749b004cb666c87bb72cb423
mark <markpackham1@gmail.com>
2023-11-21 09:35:58 +0000

    Add Github link in footer

--- frontend/src/components/sitewide/FooterSitewide.jsx
+++ frontend/src/components/sitewide/FooterSitewide.jsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 const FooterSitewide = () => {
   const currentYear = new Date(Date.now());
 
   return (
     <footer className="sticky-bottom">
       <p id="footerCopyright" className="fst-italic">
-        Copyright Mark Packham {currentYear.getFullYear()}
+        Copyright{" "}
+        <Link
+          to="https://github.com/markpackham"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mark Packham
+        </Link>{" "}
+        {currentYear.getFullYear()}
       </p>
     </footer>
   );
